6f3774db114ce9db3ee2b98040fa77da3b14e59d
RichardPalmer <richard.m.palmer@gmail.com>
2013-10-20 15:29:41 -0500

    working version

--- Trees/binary_tree.f90
+++ Trees/binary_tree.f90
@@ -15,7 +15,7 @@ module binary_tree
         integer :: count
     end type tree
 
-    public :: node, insert, search, tree, init
+    public :: node, insert, search, tree, init, print
 
 contains
 
@@ -28,23 +28,51 @@ subroutine init(t)
     !
 end subroutine init
 
-function insert(value, start) result(inserted_ptr)
+function insert(value, t) result(inserted_ptr)
     !
     integer, intent(in) :: value
-    type(node), pointer, intent(in) :: start
+    type(tree), intent(inout) :: t
+    type(node), pointer :: inserted_ptr
     !
     type(node), pointer :: x
+    !
+    x => t%head
+    inserted_ptr => insert_node(value, x)
+    if (.not. associated(t%head)) t%head => x
+    !
+end function insert
+
+function search(value, t) result(found_ptr)
+    !
+    integer, intent(in) :: value
+    type(tree), intent(inout) :: t
+    type(node), pointer :: found_ptr
+    !
+    type(node), pointer :: x
+    !
+    x => t%head
+    found_ptr => search_node(value, x)
+    !
+end function search
+
+function insert_node(value, x) result(inserted_ptr)
+    !
+    integer, intent(in) :: value
+    type(node), pointer, intent(inout) :: x
+    !
+    !type(node), pointer :: x
     type(node), pointer :: f
     type(node), pointer :: inserted_ptr
     !
-    if (associated(start)) then
-        x => start
-    else
-        nullify(x)
-    end if
+    !if (associated(start)) then
+    !    x => start
+    !else
+    !    nullify(x)
+    !end if
     nullify(f)
-    !write (*,*) 'before do loop'
+    write (*,*) 'before do loop'
     do while (associated(x))
+        write (*,*) 'in do loop'
         f => x
         if (value < x%value) then
             x => x%left
@@ -52,13 +80,13 @@ function insert(value, start) result(inserted_ptr)
             x => x%right
         end if
     end do
-    !write (*,*) 'allocate x'
+    write (*,*) 'allocate x'
     allocate(x)
     nullify(x%left)
     nullify(x%right)
-    !write (*,*) 'set value'
+    write (*,*) 'set value'
     x%value = value
-    !write (*,*) 'set f pointers'
+    write (*,*) 'set f pointers'
     if (associated(f)) then
         if (value < f%value) then
             f%left => x
@@ -66,12 +94,12 @@ function insert(value, start) result(inserted_ptr)
             f%right => x
         end if
     end if
-    !write (*,*) 'set return value'
+    write (*,*) 'set return value'
     inserted_ptr => x
     !
-end function insert
+end function insert_node
 
-function search(value, start) result(found_ptr)
+function search_node(value, start) result(found_ptr)
     !
     integer, intent(in) :: value
     type(node), pointer, intent(in) :: start
@@ -84,16 +112,31 @@ function search(value, start) result(found_ptr)
     do while (associated(x))
         write (*,*) ' *'
         if (value == x%value) then
+            write (*,'(I4,'' is equal to '',I4)') value, x%value
             exit
         else if (value < x%value) then
+            write (*,'(I4,'' is less than '',I4)') value, x%value
             x => x%left
         else
+            write (*,'(I4,'' is greater than '',I4)') value, x%value
             x => x%right
         end if
     end do
     write (*,*) 'setting return value'
     found_ptr => x
     !
-end function search
+end function search_node
+
+recursive subroutine print(x)
+    !
+    type(node), pointer, intent(in) :: x
+    !
+    if (associated(x)) then
+        call print(x%left)
+        write (*,'(I0)') x%value
+        call print(x%right)
+    end if
+    !
+end subroutine print
 
 end module binary_tree
--- Trees/main.f90
+++ Trees/main.f90
@@ -1,30 +1,38 @@
 program main
 
-    use binary_tree, only: binary_node => node, &
-        binary_insert => insert, binary_search => search, btree => tree, binary_init => init
+    use binary_tree, only: binary_node => node, btree => tree, &
+        binary_insert => insert, binary_search => search, binary_print => print, binary_init => init
 
     implicit none
 
     type(btree) :: b
     type(binary_node), pointer :: result
-    integer :: value
+    integer :: i
+    integer, dimension(10) :: vals
+    real, dimension(10) :: r
 
     write (*,'(''Hello from Tree Test'')')
-    value = 42
+    call random_number(r)
+    vals = nint(100.0 * r)
+    do i=1,size(vals)
+        write (*,'(F12.2,1X,I4)') r(i), vals(i)
+    end do
     !
     call binary_init(b)
-    result => binary_insert(value, b%head)
-    if (associated(result)) then
-        write (*,'(''inserted '',i0)') result%value
-    else
-        write (*,'(''unable to insert '',i0)') value
-    end if
-    result => binary_search(value, b%head)
-    if (associated(result)) then
-        write (*,'(''found '',i0)') result%value
-    else
-        write (*,'(''unable to find '',i0)') value
-    end if
+    do i=1,size(vals)
+        result => binary_insert(vals(i), b)
+        write (*,'(''inserted '',I4)') result%value
+    end do
+    do i=1,size(vals)
+        result => binary_search(vals(i), b)
+        if (associated(result)) then
+            write (*,'(''found '',i4)') result%value
+        else
+            write (*,'(''lost '',i4)') vals(i)
+        end if
+    end do
+    !
+    call binary_print(b%head)
     !
     write (*,'(''Goodbye'')')
 
